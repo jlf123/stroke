941735d57bafe615c5b00094f4e6c43c27490145
Edgar Gomez <egomez@atlassian.com>
2019-02-28 13:37:39 -0600

    adds a splashscreen

--- app/main.js
+++ app/main.js
@@ -1,31 +1,40 @@
 const { app, BrowserWindow } = require('electron'),
-      path = require('path'),
-      url  = require('url');
-
-const { default: installExtension, REACT_DEVELOPER_TOOLS } = require( "electron-devtools-installer" );
+    path = require('path'),
+    url = require('url');
+const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer');
+const Splashscreen = require('@trodi/electron-splashscreen');
 
 let mainWindow;
 
 const createWindow = () => {
 
+    const config = {
+        windowOpts: {
+            width: 1200, height: 800
+        },
+        templateUrl: `${__dirname}/splash-screen.html`, // ${__dirname} == Users/egomez/AtlassianDev/stroke/app
+        splashScreenOpts: {
+            width: 425,
+            height: 325
+        }
+    };
+
     installExtension(REACT_DEVELOPER_TOOLS)
         .then((name) => console.log(`Added Extension: ${name}`))
-        .catch((err) => console.log("An error occured: ", err))
+        .catch((err) => console.log('An error occured: ', err));
 
-    mainWindow = new BrowserWindow({ 
-        width: 1200, height: 800
-    });
+    mainWindow = Splashscreen.initSplashScreen(config);
 
-    mainWindow.loadURL( url.format({
-        pathname: path.join( __dirname, "index.html"),
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
         protocol: 'file:',
         slashes: true
     }));
 
     mainWindow.on('closed', () => {
         mainWindow = null;
-    })   
-}
+    });
+};
 
 // this is fired when Electron finishes initialization
 // then we create the window
@@ -35,18 +44,18 @@ app.on('ready', createWindow);
 // we don't quite the program for macOS, OS X applications usually
 // stay active until the user quit explicitly
 app.on('window-all-closed', () => {
-    if ( process.platform !== "darwin" ) { 
-        app.quit(); 
+    if (process.platform !== 'darwin') {
+        app.quit();
     }
-})
+});
 
 // this gets triggered only on macOS, it happens when we click
 // on the application's doc or taskbar icon, if no windows exist 
 // then we create one.
 app.on('activate', () => {
-    if(mainWindow === null) {
+    if (mainWindow === null) {
         createWindow();
     }
-})
+});
 
-require( "electron-debug" )(); 
+require('electron-debug')();
